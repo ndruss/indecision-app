42911a8e70dff8c8e763a8131ce5253673291c0d
nate <nate@nsideas.com>
2020-08-02 11:25:37 -0400

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,51 @@
+console.log('app.js is here');
+
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: []
+};
+
+const onFormSubmit = (e) => {
+  e.preventDefault();
+
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    renderApp();
+  }
+};
+
+const removeAll = () => {
+  app.options = [];
+  renderApp();
+};
+
+const appRoot = document.getElementById('app');
+
+const renderApp = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <p>{app.options.length}</p>
+      <ol>
+        {
+          app.options.map(option => <li key={option}>{option}</li>)
+        }
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option"/>
+        <button>Add Option</button>
+      </form>
+      <button onClick={removeAll}>Remove All</button>
+    </div>
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+renderApp();
